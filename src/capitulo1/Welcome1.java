dbd1e71c3a375712385eec0562c3008a32a2835e
yasminsantos2 <yasminsantos.eng@gmail.com>
2026-07-14 10:34:10 -0300

    feat:exemplos de saída java

---
+++ src/capitulo1/Welcome1.java
@@ -0,0 +1,7 @@
+package capitulo1;
+
+public class Welcome1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Capitulo 1");
+    }
+}
---
+++ src/capitulo1/Welcome2.java
@@ -0,0 +1,12 @@
+package capitulo1;
+
+    public class Welcome2
+    {
+        // main method begins execution of Java application
+        public static void main(String[] args)
+        {
+            System.out.print("Welcome to ");
+            System.out.println("Java Programming!");
+        } // end method main
+    } // end class Welcome2
+
